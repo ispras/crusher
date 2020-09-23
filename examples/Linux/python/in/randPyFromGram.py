06e6350c33b24c3758faa5a80b05685506a71521
Vitalii Akolzin <vva1994@ispras.ru>
2020-09-23 17:59:52 +0300

    Add Linux fuzzing examples

---
+++ examples/Linux/python/in/randPyFromGram.py
@@ -0,0 +1,93 @@
+wefo = 2 
+mdzt = 5 
+xile = 1 
+xqqt = 4 
+ercd = 5 
+tspz = 4 
+srma = 3 
+apbv = 2 
+xuxm = 4 
+tuao = 4 
+dbql = 3 
+mdys = 4 
+crvy = 4 
+yqbg = 5 
+yzrb = 1 
+fjjl = 3 
+gftu = 5 
+cgko = 3 
+hpzs = 1 
+txpf = 1 
+wsqb = 2 
+kljz = 2 
+swyb = 5 
+iypf = 2 
+weew = 3 
+kluo = 3 
+cflj = 1 
+eiti = 2 
+wdys = 3 
+ulcc = 1 
+mktx = 2 
+qdnw = 5 
+oxan = 4 
+mjyg = 3 
+prbj = 1 
+ecif = 2 
+mgei = 1 
+ydyc = 3 
+kigu = 2 
+atbz = 4 
+wlcm = 5 
+lyqd = 3 
+dfxs = 3 
+phdo = 4 
+zvbs = 1 
+uqkh = 1 
+clpm = 5 
+tdhb = 5 
+csxf = 3 
+rped = 4 
+ygyc = 2 
+nylq = 2 
+ypqd = 1 
+ctwu = 3 
+eyvy = 2 
+pugm = 4 
+ytgc = 1 
+zqaj = 5 
+
+if wefo+mdzt/xile-xqqt*ercd+95 >= tspz+86*28-87/srma :
+  
+  if 39*apbv*91/56/59+xuxm/tuao*51+13/11*13*dbql-15*mdys-99 >= 77*crvy/99*79 :
+    yqbg
+    
+  elif 77*yzrb+fjjl*7+95-gftu/cgko/49*25*77/hpzs <= txpf/wsqb+17*kljz:
+    
+    if swyb-iypf/weew*kluo+cflj <= eiti*74/71+99*39-70*wdys :
+      pass
+      
+      
+    else:
+      pass
+      
+      
+  else:
+    print "nbhmdrhpojemreu"
+    
+else:
+  
+  if ulcc+3/33-100 >= 87+15*49/20/mktx :
+    qdnw*14/oxan*mjyg/20+33
+    
+  else:
+    
+    if 3*prbj/31-52 <> 27/7+88/57*49 :
+      ecif,mgeiydyc=kigu
+      
+    elif atbz*wlcm/86*lyqd > 35*dfxs*75/81/58/91+phdo:
+      17*48/4+zvbs*uqkh/9+clpm/73
+      
+    else:
+      tdhb
+      
