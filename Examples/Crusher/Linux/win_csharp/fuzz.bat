ea8e3d3f65f34ce936d70bcdd2a855fc4b682f64
Farida <farida@star.intra.ispras.ru>
2022-10-19 17:53:51 +0300

    C# instrumentation fo windows

---
+++ Examples/Crusher/Linux/win_csharp/fuzz.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set SOURCE_DIR=%~dp0
+
+set argC=0
+for %%x in (%*) do (
+  set /A argC+=1
+)
+
+if %argC% LSS 2 (
+  echo Usage: build.bat \\path\\to\\dotnet.exe \\path\\to\\fuzz_manager \\path\\to\\dotnet
+  goto finish
+)
+
+set FUZZ_MAN=%1
+set DOTNET=%2
+
+$FUZZ_MAN --start 4 -F -i in -o out -I csharp --dotnet $DOTNET -- .\\target\\AngleSharp.Fuzz.File\\bin\\Debug\\net6.0\\AngleSharp.Fuzz.File.dll __DATA__
+
+:finish
---
+++ Examples/Crusher/Linux/win_csharp/instrument.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set SOURCE_DIR=%~dp0
+
+set argC=0
+for %%x in (%*) do (
+  set /A argC+=1
+)
+
+if %argC% LSS 1 (
+  echo Usage: build.bat /path/to/dotnet.exe
+  goto finish
+)
+
+set DOTNET=%1
+
+echo Installing SharpFuzz.CommandLine .NET tool...
+%DOTNET% tool install SharpFuzz.CommandLine --version 2.0.0 --tool-path %SOURCE_DIR%
+
+set x=0
+
+echo Instrumenting libraries...
+%SOURCE_DIR%\sharpfuzz.exe .\\target\\AngleSharp.Fuzz.File\\AngleSharp.dll
+
+
+echo Building project...
+cd .\\target\\AngleSharp.Fuzz.File
+%DOTNET% build
+cd ..\..
+
+:finish
