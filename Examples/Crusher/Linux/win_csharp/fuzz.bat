ff3ee17b407a123a48ecb52827a57cc86c31359c
Farida <farida@star.intra.ispras.ru>
2022-10-19 18:47:55 +0300

    fix batch script

--- Examples/Crusher/Linux/win_csharp/fuzz.bat
+++ Examples/Crusher/Linux/win_csharp/fuzz.bat
@@ -16,6 +16,6 @@ if %argC% LSS 2 (
 set FUZZ_MAN=%1
 set DOTNET=%2
 
-%FUZZ_MAN% --start 4 -F -i in -o out -I csharp --no-default-close-app --dotnet $DOTNET -- .\\target\\AngleSharp.Fuzz.File\\bin\\Debug\\net6.0\\AngleSharp.Fuzz.File.dll __DATA__
+%FUZZ_MAN% --start 4 -F -i in -o out -I csharp --no-default-close-app --dotnet %DOTNET% -- .\\target\\AngleSharp.Fuzz.File\\bin\\Debug\\net6.0\\AngleSharp.Fuzz.File.dll __DATA__
 
 :finish
