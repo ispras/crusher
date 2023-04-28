1b2d21988a956f237726dd31668ccfcdcbed44a6
Farida Gaifutdinova <95985213+gfarida@users.noreply.github.com>
2023-04-28 18:56:02 +0300

    add test.class src (#42)
    * add test src
    
    * fix README
    
    ---------
    
    Co-authored-by: Farida Gaifutdinova <farida.gaif@ispras.ru>

--- Examples/Crusher/Linux/java/instrumentation.sh
+++ Examples/Crusher/Linux/java/instrumentation.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-if [[ "$#" -ne 1 ]]; then
-    echo "Usage: ./instrumentation.sh <path/to/kelinci>"
+if [[ "$#" -ne 2 ]]; then
+    echo "Usage: ./instrumentation.sh <path/to/kelinci> <path/to/javac>  "
     exit 1
 fi
 
 KELINCI=$1
+JAVAC=$2
 
 mkdir bin
-cp target/test.class bin/test.class
+
+echo "compile driver"
+${JAVAC} -cp target/commons-math3-3.6.1.jar target/test.java -d bin
 
 echo "instrument project and libraries"
 
