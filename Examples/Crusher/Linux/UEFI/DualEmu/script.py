54e26ffa296d29b7ea636841854902684b46d1db
Fedor Nis'kov <fedor.niskov@ispras.ru>
2024-05-15 17:57:14 +0300

    Files for the UEFI (GRUB) examples (Nyx and Dual-Emu)

---
+++ Examples/Crusher/Linux/UEFI/DualEmu/script.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from claripy import LShR
+import dual_emu
+from dual_emu import IEmulatorWithInput, parse_args_cli
+import os, sys
+import json
+
+def entry_hook(emu: IEmulatorWithInput):
+    print('Entry Hook')
+    ptr = 0x01000000
+    sz = 0x1000
+    emu.cur_input.mark_symbolic_span(0, emu.cur_input.len())
+    emu.write_mem_b(ptr, b'\x00' * sz)
+    emu.write_mem_b(ptr, emu.cur_input.get_span(0, min(emu.cur_input.len(), sz-1)))
+    emu.write_reg("rdi", ptr)
+
+dump_path = Path(__file__).parent / 'dump' / 'info.json'
+args = parse_args_cli(input_file_required=True)
+
+ex = [0x39AA24E, 0x39A0ABD, 0x5E1D638]
+emu = dual_emu.make_emulator_with_input(args.angr, args.input, dump_file=dump_path, exits=ex,
+                                        lighthouse_out_path=args.lighthouse)
+
+if args.qiling:
+    emu.hook_fuzzer_start(emu.start_addr)
+
+emu.hook_addr(emu.start_addr, entry_hook)
+
+emu.run()
+
+if args.angr:
+    if emu.sim_man.errored:
+        print('Crashes:')
+        print(emu.sim_man.errored)
+
+    dual_emu.dump_new_inputs(emu, args.out_dir, clean=True)
