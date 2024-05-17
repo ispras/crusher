e0c69030cb99109f439442c55c6ac34d74766dcf
Fedor Niskov <fedor.niskov@ispras.ru>
2024-05-17 12:04:14 +0300

    UEFI Examples (#51)
    * Files for the UEFI (GRUB) examples (Nyx and Dual-Emu)
    
    * Update README (UEFI/NYX)
    
    * Update README (UEFI/DUALEMU)
    
    * Amend the script (UEFI/DE)

---
+++ Examples/Crusher/Linux/UEFI/Nyx/building/build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -x
+
+TYPE=$1 # "clean" or nothing
+BASE=/building # assuming running here
+
+chmod 0777 /tmp || exit 1
+apt update -y || exit 1
+apt install -y gcc build-essential uuid-dev iasl nasm python-is-python3 \
+    vim git file qemu-system ovmf dosfstools grub-efi-amd64 \
+    autoconf automake autopoint pkg-config m4 libtool flex bison gawk pev \
+    || exit 1
+
+git clone https://github.com/tianocore/edk2 || exit 1
+cd edk2 || exit 1
+git checkout edk2-stable202311  || exit 1
+git submodule update --recursive --init || exit 1
+make -C BaseTools || exit 1
+source ./edksetup.sh || exit 1
+cp $BASE/edk2_config.txt Conf/target.txt || exit 1
+build || exit 1
+cp ./Build/OvmfX64/DEBUG_GCC/FV/OVMF.fd $BASE/ovmf.fd || exit 1
+cd ..
+
+git clone https://git.savannah.gnu.org/git/grub.git || exit 1
+cd grub || exit 1
+git checkout 8719cc2040 || exit 1
+./bootstrap || exit 1
+mkdir inst || exit 1
+./configure --prefix=$(realpath inst) --with-platform=efi || exit 1
+if [ "$TYPE" != "clean" ]; then
+    cp $BASE/grub_nyx.h ./include/nyx.h || exit 1
+    patch -p1 < $BASE/grub_nyx_patch.txt || exit 1
+fi
+make -j && make install || exit 1
+./inst/bin/grub-mkstandalone -O x86_64-efi -o grub.efi || exit 1
+cd ..
+
+dd if=/dev/zero of=$BASE/boot.img bs=1M count=512 || exit 1
+mkfs.vfat $BASE/boot.img || exit 1
+mkdir $BASE/mnt || exit 1
+mount $BASE/boot.img $BASE/mnt || exit 1
+mkdir -p $BASE/mnt/efi/boot/ || exit 1
+cp $BASE/grub/grub.efi $BASE/mnt/efi/boot/bootx64.efi || exit 1
+umount $BASE/mnt || exit 1
+
+echo "OK"
+
